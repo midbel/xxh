125aeb3a3420ef805c9ce07de42292a16355e874
nico <brun.nicolas21@gmail.com>
2019-03-22 17:08:49 +0100

    update xxh command

--- cmd/xxh/main.go
+++ cmd/xxh/main.go
@@ -2,33 +2,42 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"hash"
 	"io"
-	"log"
 	"os"
 
 	"github.com/midbel/xxh"
 )
 
-func init() {
-	log.SetFlags(0)
-	log.SetOutput(os.Stdout)
-}
-
 func main() {
+	kind := flag.Uint("k", 0, "hash")
 	seed := flag.Uint("s", 0, "seed value")
 	flag.Parse()
+
+	var (
+		digest  hash.Hash
+		pattern string
+	)
+	switch *kind {
+	case 0, 64:
+		digest, pattern = xxh.New64(uint64(*seed)), "%016x %s\n"
+	case 32:
+		digest, pattern = xxh.New32(uint32(*seed)), "%08x %s\n"
+	default:
+		return
+	}
 
 	for _, f := range flag.Args() {
 		r, err := os.Open(f)
 		if err != nil {
 			continue
 		}
-		defer r.Close()
 
-		w := xxh.New32(uint32(*seed))
-		if _, err := io.Copy(w, r); err != nil {
-			continue
+		if _, err := io.Copy(digest, r); err == nil {
+			fmt.Printf(pattern, digest.Sum(nil), f)
+			digest.Reset()
 		}
-		log.Printf("%x %s", w.Sum32(), f)
+		r.Close()
 	}
 }
