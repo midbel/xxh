f9989b5c1b338bf1190632b65a60f68377fe2426
nico <brun.nicolas21@gmail.com>
2019-03-26 09:07:53 +0100

    cleaning

--- cmd/xxh/main.go
+++ cmd/xxh/main.go
@@ -7,14 +7,29 @@ import (
 	"io"
 	"os"
 	"time"
+	"runtime/pprof"
 
 	"github.com/midbel/xxh"
 )
 
 func main() {
+	cpu := flag.String("c", "", "cpu profile")
 	kind := flag.Uint("k", 0, "hash")
 	seed := flag.Uint("s", 0, "seed value")
 	flag.Parse()
+
+	if *cpu != "" {
+		w, err := os.Create(*cpu)
+		if err != nil {
+			os.Exit(5)
+		}
+		defer w.Close()
+		if err := pprof.StartCPUProfile(w); err != nil {
+			os.Exit(7)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
 	computeDigests(flag.Args(), *kind, *seed)
 }
 
@@ -22,17 +37,20 @@ func computeDigests(files []string, kind, seed uint) {
 	var (
 		digest  hash.Hash
 		pattern string
+		size int
 	)
 	switch kind {
 	case 0, 64:
 		digest, pattern = xxh.New64(uint64(seed)), "%016x  %s - %dKB (%s)\n"
+		size = 32<<10
 	case 32:
 		digest, pattern = xxh.New32(uint32(seed)), "%08x %s - %dKB (%s)\n"
+		size = 16<<10
 	default:
 		return
 	}
 
-	buffer := make([]byte, 32<<10)
+	buffer := make([]byte, size)
 	for i := 0; i < len(files); i++ {
 		r, err := os.Open(files[i])
 		if err != nil {
--- xxh.go
+++
@@ -1,7 +0,0 @@
-package xxh
-
-type Seeder interface {
-	Seed(uint)
-}
-
-var ints = []int{1, 7, 12, 18}
