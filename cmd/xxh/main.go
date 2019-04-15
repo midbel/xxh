5040eefd0251a6d1fe374391af7cff942dc3cc0a
nico <brun.nicolas21@gmail.com>
2019-04-15 10:20:23 +0200

    compare hash from list of files

--- cmd/xxh/main.go
+++ cmd/xxh/main.go
@@ -1,68 +1,113 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"hash"
 	"io"
 	"os"
-	"runtime/pprof"
-	"time"
 
 	"github.com/midbel/xxh"
 )
 
 func main() {
-	cpu := flag.String("c", "", "cpu profile")
+	cmp := flag.Bool("c", false, "compare digest")
 	kind := flag.Uint("k", 0, "hash")
 	seed := flag.Uint("s", 0, "seed value")
 	flag.Parse()
 
-	if *cpu != "" {
-		w, err := os.Create(*cpu)
-		if err != nil {
-			os.Exit(5)
-		}
-		defer w.Close()
-		if err := pprof.StartCPUProfile(w); err != nil {
-			os.Exit(7)
-		}
-		defer pprof.StopCPUProfile()
+	var err error
+	if *cmp {
+		err = compareDigests(flag.Arg(0), *kind, *seed)
+	} else {
+		err = computeDigests(flag.Args(), *kind, *seed)
 	}
-
-	computeDigests(flag.Args(), *kind, *seed)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
 }
 
-func computeDigests(files []string, kind, seed uint) {
+var buffer = make([]byte, 8<<10)
+
+func compareDigests(file string, kind, seed uint) error {
+	r, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	var digest  hash.Hash
+	switch kind {
+	case 0, 64:
+		digest = xxh.New64(uint64(seed))
+	case 32:
+		digest = xxh.New32(uint32(seed))
+	default:
+		return fmt.Errorf("unknown hash version")
+	}
+	rs := bufio.NewReader(r)
+
+	var errcount int
+	for {
+		var want, file string
+		n, err := fmt.Fscanf(rs, "%s  %s\n", &want, &file)
+		if n != 2 && err == nil {
+			return fmt.Errorf("not enough element scanned")
+		}
+		switch err {
+		case nil:
+			sum, err := calculate(file, digest)
+			if err != nil {
+				continue
+			}
+			if hs := fmt.Sprintf("%016x", sum); hs != want {
+				errcount++
+				fmt.Fprintf(os.Stderr, "%s: digest does not match\n", file)
+			}
+		case io.EOF:
+			if errcount == 0 {
+				fmt.Fprintln(os.Stdout, "OK")
+			}
+			return nil
+		default:
+			return err
+		}
+	}
+}
+
+func computeDigests(files []string, kind, seed uint) error {
 	var (
 		digest  hash.Hash
 		pattern string
-		size    int
 	)
 	switch kind {
 	case 0, 64:
-		digest, pattern = xxh.New64(uint64(seed)), "%016x  %s - %dKB (%s)\n"
-		size = 32 << 10
+		digest, pattern = xxh.New64(uint64(seed)), "%016x  %s\n"
 	case 32:
-		digest, pattern = xxh.New32(uint32(seed)), "%08x %s - %dKB (%s)\n"
-		size = 16 << 10
+		digest, pattern = xxh.New32(uint32(seed)), "%08x  %s\n"
 	default:
-		return
+		return fmt.Errorf("unknown hash version")
 	}
-
-	buffer := make([]byte, size)
 	for i := 0; i < len(files); i++ {
-		r, err := os.Open(files[i])
-		if err != nil {
-			continue
+		sum, err := calculate(files[i], digest)
+		if err == nil {
+			fmt.Printf(pattern, sum, files[i])
 		}
-
-		when := time.Now()
-		if n, err := io.CopyBuffer(digest, r, buffer); err == nil {
-			sum := digest.Sum(nil)
-			fmt.Printf(pattern, sum, files[i], n>>10, time.Since(when))
-			digest.Reset()
-		}
-		r.Close()
 	}
+	return nil
+}
+
+func calculate(file string, digest hash.Hash) ([]byte, error) {
+	defer digest.Reset()
+
+	r, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+
+	_, err = io.CopyBuffer(digest, r, buffer)
+	return digest.Sum(nil), err
 }
